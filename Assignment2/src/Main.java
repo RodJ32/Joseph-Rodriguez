d563fa234ef6205ccfc4d97dbcb921cd48b4d652
Joseph Rodriguez <jrodr332@depaul.edu>
2025-09-30 18:22:33 -0500

    Task 3: SeLinkedList added with linked node

--- Assignment2/src/Main.java
+++ Assignment2/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main: Task 2 - Test Players");
-        Player p = new Player();
-        p.testPrint();
+        System.out.println("Main: Task 3");
+        SeLinkedList n = new SeLinkedList(1, 2, 3);
+        System.out.println("Created: " + n);
         System.out.println("Completed");
     }
 }
