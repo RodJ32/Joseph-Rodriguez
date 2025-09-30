76479e5a05496b07c1a11d52287edf5df82ecaca
Joseph Rodriguez <jrodr332@depaul.edu>
2025-09-30 18:10:13 -0500

    Task 2: Added Player class with numPlayer int and tested

--- Assignment2/src/Main.java
+++ Assignment2/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello there!");
+        System.out.println("Main: Task 2 - Test Players");
+        Player p = new Player();
+        p.testPrint();
+        System.out.println("Completed");
     }
 }
---
+++ Assignment2/src/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    public int numPlayers;
+    
+    public Player() {
+        this.numPlayers = 0;
+    }
+
+    public void testPrint() {
+        System.out.println("Player.testPrint(): numPlayers=" + numPlayers);
+    }
+}
